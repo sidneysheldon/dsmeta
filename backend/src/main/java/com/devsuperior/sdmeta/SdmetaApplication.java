395ff024482567db177cfc3b6cae400549d198e6
Sidney Sheldon <sheldon.jampa80@gmail.com>
2022-11-15 12:02:30 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/sdmeta/SdmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/sdmeta/SdmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
